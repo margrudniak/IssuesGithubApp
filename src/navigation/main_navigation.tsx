02ec61bcd1529e86f382796ebf654231c697bec6
Marcin Grudniak <maverickmonkey69@gmail.com>
2021-09-26 22:03:50 +0200

    Navigation main&element tree

---
+++ src/navigation/main_navigation.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {ElementScreen, MainScreen} from '../containers';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator();
+
+export const MainNavigation = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Main" component={MainScreen} />
+          <Stack.Screen name="Element" component={ElementScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
